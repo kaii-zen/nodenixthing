99d64976c4797834c987c76753c6d6701203d3f5
Shay Bergmann <shaybergmann@gmail.com>
2018-04-18 19:30:58 -0400

    remove dead comment

--- test/hellojs/default.nix
+++ test/hellojs/default.nix
@@ -1,10 +1,5 @@
 import ../../default.nix {
-  #src = builtins.path {
-  #  path = ./.;
-  #  filter = path: type: type != "symlink" && ! builtins.elem (baseNameOf path) [ ".git" "node_modules" ];
-  #};
   srcPath = ./.;
-
   npmPkgOpts = {
     "hello:meow" = "woof";
   };
