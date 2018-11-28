af97a9f6fdde445ca850999b64f0667d03e8a7cf
Shay Bergmann <sbergmann@benbria.ca>
2018-11-28 11:35:55 -0500

    Format

---
+++ pkgs/make-context/default.nix
@@ -0,0 +1,20 @@
+{ callPackage, writeText, lib }:
+{ package, lock, supplemental ? {}}:
+
+with builtins;
+assert isAttrs package;
+assert isAttrs lock;
+assert isAttrs supplemental;
+
+with lib;
+let
+  input = makeExtensible (_: {
+    inherit package lock supplemental;
+  });
+
+  transforms = callPackage ./transformations.nix {};
+
+  inputTransformed = input.extend transforms;
+  context          = inputTransformed.dependencies;
+
+in context
